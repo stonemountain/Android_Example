20b1c9413a907777378473ffcb28769e064d94e6
shangsong <shangs@smvp.cn>
2015-03-09 16:18:24 +0800

    SmvpDemo: Delete the token
    Signed-off-by: shangsong <shangs@smvp.cn>

--- For_AndroidStudio/app/src/main/java/cn/smvp/sdk/demo/util/LocalConstants.java
+++ For_AndroidStudio/app/src/main/java/cn/smvp/sdk/demo/util/LocalConstants.java
@@ -6,5 +6,5 @@ package cn.smvp.sdk.demo.util;
 public interface LocalConstants {
     String ACTION_GET_ALL_VIDEOS = "com.smvp.testapi.get.videos";
     String ACTION_GET_ALL_VIDEOS_COMPLETED = "com.smvp.testapi.get.videos.completed";
-    String SMVP_TOKEN = "P6L-OXvNyZjVAutQvdY1b4MNXRZyLlaSwjYBhD0YA3wzs1ZipMxUCc3iUrtbL98r";
+    String SMVP_TOKEN = "";
 }
