63be020ab79ebc84cda54e18d827774ce7ad9212
shangsong <shangs@smvp.cn>
2015-08-06 08:18:41 +0800

    SmvpDemo: Update code
    Signed-off-by: shangsong <shangs@smvp.cn>

--- For_AndroidStudio/app/src/main/java/cn/smvp/sdk/demo/util/LocalConstants.java
+++ For_AndroidStudio/app/src/main/java/cn/smvp/sdk/demo/util/LocalConstants.java
@@ -4,10 +4,6 @@ package cn.smvp.sdk.demo.util;
  * Created by shangsong on 14-9-23.
  */
 public interface LocalConstants {
-    //生产环评  shangsong
-    String TOKEN = "2Oe2-V4R1GtQF2ZhtxJc297qxkMOKN1XVxarsiMmkeVk302fdSGiF1Qs0LsAzE8P";
-    String PLAYER_ID = "640361859865669265";
-    //生产环境 admin
-//    String TOKEN = "SJQ-7zgT_lOwaq816PPkkat-xX-Wsdo9Rj-STfU4gvs=";
-//    String PLAYER_ID = "428082998184697028";
+    String TOKEN = "";
+    String PLAYER_ID = "";
 }
